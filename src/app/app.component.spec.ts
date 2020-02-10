dd2ae5d1c7a64310c8634d35fc276a266f5d4bb1
cgobbet <cassiano.gobbet@gmail.com>
2020-02-10 21:33:05 +0100

    Build deployment

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Ace of Spades is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Human Rights Quiz is running!  Hail HRQ!');
   });
 });
